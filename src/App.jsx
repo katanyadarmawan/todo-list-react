4dbb40425d493918c264e5764f58bd53b84d5416
Rizky Darmawan <rizky.darmawan@gli.id>
2025-02-09 20:36:02 +0700

    update fitur onchange

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,38 @@ import "./App.css";
 function App() {
   const [lists, setLists] = useState([]);
   const [todo, setTodo] = useState("");
+  const [tempTodo, setTempTodo] = useState({});
   const [index, setIndex] = useState(null);
-  const [isEdit, setIsEdit] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
 
   const addTodo = () => {
     if (todo === "") return;
     if (index != null) {
-      console.log(1);
       lists[index] = todo;
     } else {
-      console.log(2);
-      const newData = [...lists, todo];
+      const newData = [...lists, { todo: todo, status: false }];
       setLists(newData);
     }
     setTodo("");
-    setIsEdit(false);
     setIndex(null);
   };
 
   const editTodo = (data) => {
     const newEditData = lists.findIndex((e) => e === data);
     setIndex(newEditData);
-    setTodo(data);
-    setIsEdit(true);
+    setTempTodo(data);
+    setIsUpdate(true);
   };
 
+  const updateTodo = (index) => {
+    lists[index] = tempTodo;
+    setIsUpdate(false);
+    setIndex(null);
+  };
+
+  const handleChangeTodo = (data) => {
+    setTempTodo((prev) => ({ ...prev, todo: data.target.value }));
+  };
   const deleteTodo = (data) => {
     setLists((prev) => prev.filter((e) => e != data));
   };
@@ -47,7 +54,7 @@ function App() {
           type="button"
           onClick={addTodo}
         >
-          {isEdit ? "Edit" : "Add"}
+          Add
         </button>
       </div>
       <div className="header mb-5">
@@ -61,26 +68,50 @@ function App() {
             <thead>
               <tr>
                 <th className="border border-gray-300 w-50">Todo</th>
-                <th className="border border-gray-300 ">Action</th>
+                <th className="border border-gray-300 w-50">Status</th>
+                <th className="border border-gray-300 w-50">Action</th>
               </tr>
             </thead>
             <tbody>
               {lists.map((e, i) => (
                 <tr key={i}>
-                  <td className="border border-gray-300 ">{e}</td>
+                  <td className="border border-gray-300 ">
+                    {isUpdate && index === i ? (
+                      <input
+                        type="text"
+                        className="border-b-2 outline-0"
+                        value={tempTodo.todo}
+                        onChange={handleChangeTodo}
+                      />
+                    ) : (
+                      e.todo
+                    )}
+                  </td>
+                  <td className="border border-gray-300 ">{e.status}</td>
                   <td className="border border-gray-300 p-2">
-                    <button
-                      className="rounded-md w-15 bg-yellow-500 text-white cursor-pointer"
-                      onClick={() => editTodo(e)}
-                    >
-                      Edit
-                    </button>{" "}
-                    <button
-                      className="rounded-md w-15 bg-red-500 text-white cursor-pointer"
-                      onClick={() => deleteTodo(e)}
-                    >
-                      Delete
-                    </button>
+                    <div className="action">
+                      {isUpdate && index === i ? (
+                        <button
+                          className="rounded-md w-15 bg-yellow-500 text-white cursor-pointer mr-2"
+                          onClick={() => updateTodo(i)}
+                        >
+                          Update
+                        </button>
+                      ) : (
+                        <button
+                          className="rounded-md w-15 bg-yellow-500 text-white cursor-pointer mr-2"
+                          onClick={() => editTodo(e)}
+                        >
+                          Edit
+                        </button>
+                      )}
+                      <button
+                        className="rounded-md w-15 bg-red-500 text-white cursor-pointer"
+                        onClick={() => deleteTodo(e)}
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </td>
                 </tr>
               ))}
